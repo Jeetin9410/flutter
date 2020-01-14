bd821b037d887a0a116328dc32701dce7d151027
Jeetin kumar <kumaranand8126@gmail.com>
2020-01-14 12:55:07 +0530

    StatefulWidget Testing

---
+++ lib/statefulWidget.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+
+}
+
+class _MyAppState extends State<MyApp>{
+  String name = '';
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Stateful Widget Testing"),
+        ),
+        body:
+        Column(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(10.0),
+              alignment: Alignment.center,
+             child:Column(
+               children: <Widget>[
+                 TextField(
+                   onChanged: (String string){
+                     setState(() {
+                       name = string;
+                     });
+                   },
+                 ) ,
+
+               ],
+             )
+            ),
+            Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Text("Your text is1 $name"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
